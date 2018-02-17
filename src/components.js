53c390b0e5fc3ba1a23bcc69d85a82e7b2b3e881
Goeff Thomas <goeff@starbly.com>
2018-02-17 13:57:28 -0800

    Planning out structure

---
+++ src/components.js
@@ -0,0 +1,14 @@
+export class AppModule {
+    headerTemplate = `
+        <header>
+            <a><img src=
+    `;
+}
+
+export class StoryModule {
+    
+}
+
+export class CommentModule {
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './styles.scss';
+import { AppModule, StoryModule, CommentModule } from './components';
 document.getElementById('app').innerText = 'HACKER NEWS!';
